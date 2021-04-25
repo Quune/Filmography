882bff7b06c95f74741e323b8c29c34a88650721
Quune <quentin.geliot@yahoo.fr>
2021-04-25 17:16:31 +0200

    feat(angular) : add component files

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FilesListComponent } from './components/files-list/files-list.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'files', pathMatch: 'full' },
+  { path: 'files', component: FilesListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ Frontend/src/app/components/files-list/files-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-list',
+  templateUrl: './files-list.component.html',
+  styleUrls: ['./files-list.component.css']
+})
+export class FilesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
